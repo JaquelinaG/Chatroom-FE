8c84de45e14e14fbbe5a18526530d3220541a513
JaquelinaG <jaquelinagiuliani@gmail.com>
2019-11-02 17:24:44 -0300

    Add chatroom component to module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ChatComp } from './components/chat.comp';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComp
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {path: '', component: ChatComp},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
